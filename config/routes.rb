de1971bea2e1747011a8d5cc9cd4deb4e85dc96f
John Slota <john.slota@gmail.com>
2016-03-03 22:52:51 -0700

    this is actually waffle ticket 2-oauth-with-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/", to: 'welcome#index'
-  get "/auth/google_oauth2", as: :login
-  get "/auth/google_oauth2/callback", to: "sessions#create"
+  get "/auth/facebook", as: :login
+  get "/auth/facebook/callback", to: "sessions#create"
   get '/logout', to: 'sessions#destroy'
   resources :welcome, only: [:index, :show]
   resources :legislators, only: [:index, :show]
