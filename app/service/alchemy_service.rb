a7da9538d788c858c33fffb5c7fb79ecb84020a4
John Slota <john.slota@gmail.com>
2016-03-06 12:06:03 -0700

    api endpoint being hit but locked out for day due to rate limit

--- app/service/alchemy_service.rb
+++ app/service/alchemy_service.rb
@@ -2,13 +2,11 @@ class AlchemyService
   attr_reader :connection
 
   def initialize
-    binding.pry
-    @connection = Excon.new(url: "https://gateway-a.watsonplatform.net/calls/data/GetNews/")
+    @connection = Faraday.new(url: "https://gateway-a.watsonplatform.net/calls/data/GetNews")
   end
 
   def get_news(name)
     binding.pry
-    Faraday.default_adapter = :excon
-    JSON.parse(connection.get("apikey=#{ENV["ALCHEMY_API_KEY"]}&outputMode=json&start=now-7d&end=now&maxResults=25&return=enriched.url.title,enriched.url.url,enriched.url.publicationDate&q.enriched.url.entities.entity=|text=#{name[0]}%20#{name[1]},type=person|&q.enriched.url.docSentiment.type=negative").body)
+    JSON.parse(connection.get("?apikey=#{ENV["ALCHEMY_API_KEY"]}&outputMode=json&start=now-1d&end=now&maxResults=5&return=enriched.url.title,enriched.url.url,enriched.url.publicationDate&q.enriched.url.entities.entity=|text=#{name[0]}%20#{name[1]},type=person|&q.enriched.url.docSentiment.type=negative").body)
   end
 end
