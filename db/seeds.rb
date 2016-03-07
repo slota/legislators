d873e4eaa1bbe39c4adbdd87e6dd3bb52be293b6
John Slota <john.slota@gmail.com>
2016-03-06 18:31:56 -0700

    presidents being seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+President.create(name: "Bernie Sanders", image: "http://powderedwigsociety.com/wp-content/uploads/2016/02/socialist-bernie-sanders.jpg", url: "https://berniesanders.com/?nosplash=true")
+President.create(name: "Donald Trump", image: "http://a.abcnews.go.com/images/Politics/GTY_donald_trump_ml_160304_12x5_1600.jpg", url: "https://www.donaldjtrump.com/")
+President.create(name: "Hillary Clinton", image: "http://freebeacon.com/wp-content/uploads/2015/07/Hillary-Clinton1.jpg", url: "https://www.hillaryclinton.com/")
